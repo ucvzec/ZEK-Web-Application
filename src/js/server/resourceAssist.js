592dfe93735180d379eeb719194101d9ff7a61e9
zeno-vg <michaelkovarschool@gmail.com>
2018-10-27 17:10:49 -0500

    Disabled Resourece Assist Dev Mode

--- src/js/server/resourceAssist.js
+++ src/js/server/resourceAssist.js
@@ -2,12 +2,13 @@
 	Depedencies
 */
 const fs = require("fs");
-
+//toggles on and off the dev settings
+const dev = false;
 /*
 	Module features
 */
 function sendFileIfExists(path,res){
-	console.log(`Path: ${path}`);
+	(dev?console.log(`Path: ${path}`):"");
 	if(fs.existsSync(path)){
 		res.sendFile(path);
 	}else{
